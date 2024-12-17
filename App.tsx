0e4cb1080e4f0757d47bdb72a2339c5602f7ecd1
louis burrows <louisburrows@gmail.com>
2024-12-17 21:23:48 +0100

    restored async

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { AppNavigator } from './navigation/AppNavigator';
-import { TodoProvider } from './context/TodoContext';
 
 export default function App() {
   return (
-    <TodoProvider>
+    <>
       <AppNavigator />
-    </TodoProvider>
+    </>
   );
 }
