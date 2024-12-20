1279946f07e28c901893fa856f0022fc88ee5169
louis burrows <louisburrows@gmail.com>
2024-12-20 01:55:34 +0100

    Update TodoItem.tsx

--- screens/TodoListScreen/components/TodoItem/TodoItem.tsx
+++ screens/TodoListScreen/components/TodoItem/TodoItem.tsx
@@ -3,7 +3,6 @@ import { View, Text, Button } from 'react-native';
 import { Checkbox } from 'react-native-paper';
 import styles from './TodoItem.styles';
 
-
 // Import Types
 import { TodoType } from '../../../../types';
 
@@ -28,7 +27,7 @@ const TodoItem: React.FC<TodoItemProps> = ({
           onPress={() => onToggleCompleted(item.id)}
           color={item.completed ? styles.completedCheckbox.color : undefined}
         />
-        <Text 
+        <Text
           style={[styles.text, item.completed && styles.completedText]}
           numberOfLines={3}
         >
@@ -39,10 +38,10 @@ const TodoItem: React.FC<TodoItemProps> = ({
       {item.completed ? (
         <Text style={styles.wellDoneText}>Well Done</Text>
       ) : (
-        <Button 
-          title="Remove" 
-          onPress={() => onRemove(item.id)} 
-          color={styles.removeButton.color} 
+        <Button
+          title="Remove"
+          onPress={() => onRemove(item.id)}
+          color={styles.removeButton.color}
         />
       )}
     </View>
